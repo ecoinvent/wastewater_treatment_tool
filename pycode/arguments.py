954fa290f9507c7e175ce792da7c55e6c96c674e
Pascal Lesage <pascal.lesage@polymtl.ca>
2018-03-22 20:16:22 -0400

    arguments list gets longer

--- pycode/arguments.py
+++ pycode/arguments.py
@@ -1,5 +1,31 @@
 """ List of arguments that must be passed by the ICRA tool"""
+required_arguments = [
+    "root_dir", # Root directory of data and code
+    "tool_use_type", # "average" or specific
+    "untreated_fraction", #float, [0, 1]
+    "overload_loss_fraction_particulate", #float, [0, 1]
+    "overload_loss_fraction_dissolved", #float, [0, 1]
+    "WW_type", #str, from (1) in http://84.89.61.64:8030/ecoinvent/simplified_data_entry.php
+    "geography", #str
+    "timePeriodStart", #TODO --> Should we use defaults (which?) or ask for this data in tool?
+    "timePeriodEnd", #TODO --> Should we use defaults (which?) or ask for this data in tool?
+    
+other_args = {
+    'specific': [
+        "technology", # str, TODO
+        "capacity", # Class1, Class2, etc.
+    ],
+    'average': [
+    ]
+}
+        
 
+required_arguments_specific_use = [
+    "technology", # str, TODO
+    "capacity", # Class1, Class2, etc.
+    
+
+    
 
 ########################################################################
 # General
@@ -16,9 +42,11 @@ root_dir = r'C:\mypy\code\wastewater_treatment_tool'
 ########################################################################
 # High-level parameters
 ########################################################################
+tool_use_type = "average" # or specific. 
+
 untreated_fraction = 0.2 # float, represents the fraction emitted directly to the environment
-overload_loss_fraction_particulate = 0.01
-overload_loss_fraction_dissolved = 0.02
+overload_loss_fraction_particulate = 0.01 # float
+overload_loss_fraction_dissolved = 0.02 # float
 
 ########################################################################
 # Metadata
