3ca0b69c49fac7e147170dd5ec68faad717acc1c
Pascal Lesage <pascal.lesage@polymtl.ca>
2018-03-23 15:44:59 -0400

    Update init

--- pycode/__init__.py
+++ pycode/__init__.py
@@ -1,7 +1,13 @@
-from .utils import pkl_dump, list_to_df, is_empty, build_file_list, dataframe_to_excel, find_youngest, pkl_load, add_path_in_list
+from .utils import *
 
 from .load_master_data import load_MD
+from .WWecoSpoldGenerator import DirectDischarge_ecoSpold, WWT_ecoSpold
+from .defaults import *
+from .placeholders import *
+from .spold_utils import *
 
 __all__ = [
-    'load_MD',  
+    'load_MD',
+    'DirectDischarge_ecoSpold',
+    'WWT_ecoSpold',
 ]
