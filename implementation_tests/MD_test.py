3baa96cc864b27c42461286ca1bd90b11a5ae9d6
Pascal Lesage <pascal.lesage@polymtl.ca>
2018-03-23 11:50:22 -0400

    MD_test update

--- implementation_tests/MD_test.py
+++ implementation_tests/MD_test.py
@@ -1,23 +1,14 @@
 import os
+import sys 
 
+os.chdir(r'..')
+root_dir = os.getcwd()
+sys.path.append(root_dir)
 
-########################################################################
-# Arguments
-########################################################################
+from pycode import load_MD
 
-# Root directory, where all files and code will be found
-root_dir = r'C:\mypy\code\wastewater_treatment_tool'
-
-########################################################################
-# Function
-########################################################################
-
-if __name__ is '__main__':
-    os.chdir(root_dir)
-    from pycode import load_MD
-
-    load_MD(root_dir)
-            
-    assert os.path.isfile(os.path.join(root_dir, 'MasterData', 'MD.pkl')), "MD.pkl was not saved"
-    assert os.path.isfile(os.path.join(root_dir, 'MasterData', 'xlsx', 'MasterData_fields.xlsx')), "MasterData_fields.xlsx was not saved"
-    print("Test passed")
+load_MD(root_dir)
+        
+assert os.path.isfile(os.path.join(root_dir, 'MasterData', 'MD.pkl')), "MD.pkl was not saved"
+assert os.path.isfile(os.path.join(root_dir, 'MasterData', 'xlsx', 'MasterData_fields.xlsx')), "MasterData_fields.xlsx was not saved"
+print("Test passed")
